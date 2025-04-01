1c9b740493fa41e4f31b3b3322225a85b17ca2fc
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-01 16:19:49 +0700

    update main.ts to add hbs and hbs utils module

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import { AppModule } from "./app.module";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { join } from "path";
 
+import * as hbs from "hbs";
+import * as hbsUtils from "hbs-utils";
+
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 	app.useStaticAssets(join(__dirname, "..", "public"));
 	app.setBaseViewsDir(join(__dirname, "..", "views"));
+	hbs.registerPartials(join(__dirname, "..", "views/layouts"));
+	hbsUtils(hbs).registerWatchedPartials(join(__dirname, "..", "views/layouts"));
+
 	app.setViewEngine("hbs");
 
 	await app.listen(3000);
