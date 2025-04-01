7667bbbc46e497a9614a481d7a7f2d1c77798b1f
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-01 17:32:55 +0700

    update app controller to include data on home page and about route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,5 +4,24 @@ import { Controller, Get, Render } from "@nestjs/common";
 export class AppController {
 	@Get("/")
 	@Render("index")
-	index() {}
+	index() {
+		return {
+			title: "Home Page - Oneline Store",
+		};
+	}
+
+	@Get("/about")
+	@Render("about")
+	about() {
+		const viewData = [];
+		viewData["description"] = "This is an about page...";
+		viewData["author"] = "Developed by Toan";
+		const data1 = "About us - Online Store";
+
+		return {
+			title: data1,
+			subtitle: "About us",
+			viewData: viewData,
+		};
+	}
 }
